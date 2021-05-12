68932b189be0f762c885acc06bd2118e6bcda08f
Javier Pardo <47202605+JaviPardox@users.noreply.github.com>
2021-05-12 02:00:46 +0200

    add multiple functionalities
    index.html -> changed d3 script 
    style.css -> added styling & fixed mapid id
    logic.js -> added dot marks and legend

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "pk.eyJ1IjoiamF2aXBhcmRvIiwiYSI6ImNrb2trM3F0NjA2YzMybnBlZGcwN2c5c3cifQ.dxmZk4DZxoXQHwjgzCDMDw";
--- static/js/logic.js
+++ static/js/logic.js
@@ -45,6 +45,83 @@ let overlays = {
 L.control.layers(baseMaps. overlays).addTo(map)
 
 // Retrieve earthquake data
-d3.json('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson').then(function(data){
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data){
+
+    // Funtion to return style for each earthquake we plot on the map
+    function styleInfo(feature){
+        return{
+            opacity: 1,
+            fillOpacity: 1,
+            fillColor: getColor(feature.properties.mag),
+            color: '#000000',
+            radius: getRadius(feature.properties.mag),
+            stroke: true,
+            weight: 0.5
+        }
+    }
+
+    function getColor(magnitude){
+
+        if(magnitude > 5){
+            return "#ea2c2c"
+        }
+        if(magnitude > 4){
+            return "#ea822c"
+        }
+        if(magnitude > 3){
+            return "#ee9c00"
+        }
+        if(magnitude > 2){
+            return "#eecc00"
+        }
+        if(magnitude > 1){
+            return "#d4ee00"
+        }
+        return "#98eee0"
+
+    }
+
+    // Determine the radius of the earthquake's marker depending on its magnitude
+    function getRadius(magnitude){
+
+        if(magnitude === 0){
+            return 1
+        }
+        return magnitude * 4;
+
+    }
+
+    L.geoJson(data, {
+        pointToLayer: function(feature, latlng){
+            console.log(data)
+            return L.circleMarker(latlng)
+        },
+        style: styleInfo,
+        onEachFeature : function(feature, layer){
+            layer.bindPopup("Magnitude: " + feature.properties.mag + '<br>Location: ' + feature.properties.place)
+
+        }
+    }).addTo(allEarthquakes)
+
+    allEarthquakes.addTo(map)
+
+    let legend = L.control({
+        position: 'bottomright'
+    })
+
+    legend.onAdd = function(){
+        let div = L.DomUtil.create('div', 'info legend')
+        const magnitudes = [0,1,2,3,4,5]
+        const colors = ["#98eee0","#d4ee00","#eecc00","#ee9c00","#ea822c","#ea2c2c"]
+
+        for(var i = 0; i < magnitudes.length; i++){
+            console.log(colors[i])
+            div.innerHTML += `<i style='background:${colors[i]}'></i>` + magnitudes[i] + (magnitudes[i + 1] ? '&ndash;' + magnitudes[i + 1] + '<br>': '+')
+        }
+        return div
+    }
+
+    legend.addTo(map)
+
 
 })
